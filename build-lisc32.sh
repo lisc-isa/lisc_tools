a77c236c8ee01c50c393ce85b87e6c27c1d55394
Dong Tong <dthome9180@gmail.com>
2018-07-06 13:39:48 +0800

    add build-lisc32.sh

---
+++ build-lisc32.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+#
+# Script to build LISC ISA simulator, proxy kernel, and GNU toolchain.
+
+. build.common
+
+echo "Starting LISC Toolchain build process"
+
+cd ./patch && tar -mvzcf lisc-patch.tar.gz riscv spike_main && cd ..
+
+if [ ! -e fesvr ]
+then
+  (
+  git clone https://github.com/riscv/riscv-fesvr.git fesvr
+  )
+fi
+if [ ! -e isa-sim ]
+then
+  (
+  git clone https://github.com/riscv/riscv-isa-sim.git isa-sim
+  )
+fi
+cd isa-sim
+git checkout -f 2dbcb01
+
+tar -mxvf ../patch/lisc-patch.tar.gz
+cp -f riscv/encoding.h ../fesvr/fesvr/encoding.h 
+rm -rf ../patch/lisc-patch.tar.gz
+cd ..
+
+build_project fesvr --prefix=$LISC
+build_project isa-sim --prefix=$LISC --with-fesvr=$LISC
+
+if [ ! -e toolchain]
+then
+  (
+  git clone https://github.com/lisc-isa/lisc_gnu_toolchain toolchain
+  )
+fi
+cd toolchain && make && cd ..
+
+echo -e "\\nlisc spike installation completed!"
